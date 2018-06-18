1a73ad8caeddbba0820afc95e5901263875350be
noma <harinezumilove1106@gmail.com>
2018-06-18 20:34:23 +0900

    14.8

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   foreign_key: "follower_id",
   dependent:   :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
+  has_many :following, through: :active_relationships, source: :followed
+
   before_save { self.email = email.downcase }
   before_save   :downcase_email
   before_create :create_activation_digest
